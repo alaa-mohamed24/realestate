cba79ff19762e77c979bdd6fcaf86650a3a4b868
alaa-mohamed24 <lily.m.soby24@gmail.com>
2025-06-04 19:26:17 +0300

    Add root URL redirect to /properties/

--- realestate_project/urls.py
+++ realestate_project/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('properties/', include('properties.urls')), 
+    path('', RedirectView.as_view(url='properties/')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
